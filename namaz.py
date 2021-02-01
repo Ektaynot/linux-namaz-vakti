f3ab0407b501e167ce7b9d432e9f9b2e97872a39
Ektaynot <40837206+Ektaynot@users.noreply.github.com>
2021-02-02 00:52:59 +0300

    Update namaz.py

--- namaz.py
+++ namaz.py
@@ -16,19 +16,33 @@ conn = http.client.HTTPSConnection("api.collectapi.com")
 
 headers = {
     'content-type': "application/json",
-    'authorization': "apikey xxxxxxx"
+    'authorization': "apikey xxxxx"
     }
 
 data=""
 
 def vakit():
-    conn.request("GET", "/pray/all?data.city=istanbul", headers=headers)
+    conn.request("GET", "/pray/all?data.city=kocaeli", headers=headers)
     res = conn.getresponse()
     data = res.read()
     file2write=open(f"{yer}",'a')
     file2write.write(f'{(data.decode("utf-8"))}\n')
     file2write.close()
     print("oluşturuldu")
+
+def hesap(farki,suan,kime,saatkaca):
+    if farki<60:
+        dakika=f'{farki} dakika'
+    elif farki>60 and farki<120:
+        dakika=f"1 saat {farki-60} dakika"
+    elif farki>120 and farki<180:
+        dakika=f"2 saat {farki-120} dakika"
+    elif farki>180 and farki<240:
+        dakika=f"3 saat {farki-180} dakika"
+    elif farki>240 and farki<300:
+        dakika=f"4 saat {farki-240} dakika"
+    os.system(f"notify-send 'Şu An {suan}' '{kime} Saati:{saatkaca} | Kalan Süre {dakika}'")
+
 
 x = datetime.datetime.now()
 logloc2=os.path.expanduser(f'~/.cache/namaz/{x.strftime("%Y-%m-%d")}.txt')
@@ -49,26 +63,29 @@ def bildirim():
     ikindi2 = int(ikindi[0:2])*60 + int(ikindi[3:5])
     aksam2 = int(aksam[0:2])*60 + int(aksam[3:5])
     yatsı2 = int(yatsı[0:2])*60 + int(yatsı[3:5])
+    if saat[1]==":":
+        saat="0"+saat
     zaman=int(saat[0:2])*60 + int(saat[3:5])
+
     if zaman < imsak2 or zaman>yatsı2:
         os.system(f"notify-send 'Şu An Yatsı' 'İmsak Saati:{imsak}'")
-        print("yatsı")
-    elif zaman < gunes2 and zaman>imsak2:
-        os.system(f"notify-send 'Şu An İmsak' 'Güneş Saati:{gunes}'")
+            
+    elif zaman < ogle2 and zaman>imsak2:
+        hesap(ogle2-zaman,"İmsak","Öğle",ogle)
         print("imsak")
-    elif zaman < ogle2 and zaman>gunes2:
-        os.system(f"notify-send 'Şu An Ogle' 'İkindi Saati:{ikindi}'")
-        print("ogle")
     elif zaman < ikindi2 and zaman>ogle2:
-        os.system(f"notify-send 'Şu An İkindi' 'Akşam Saati:{aksam}'")
-        print("ikindi")
-    elif zaman < yatsı2 and zaman>ikindi2:
-        os.system(f"notify-send 'Şu An Akşam' 'Yatsı Saati:{yatsı}'")
-        print("aksam")
+        hesap(ikindi2-zaman,"Öğle","İkindi",ikindi)
+        print("ogle")
+    elif zaman < aksam2 and zaman>ikindi2:
+        hesap(aksam2-zaman,"İkindi","Akşam",aksam)
+    elif zaman < yatsı2 and zaman>aksam2:
+        hesap(yatsı2-zaman,"Akşam","Yatsı",yatsı)
+        print("aksam",saat)
 
 if os.path.exists(logloc2) == False:
     yer=os.path.expanduser(f'~/.cache/namaz/{x.strftime("%Y-%m-%d")}.txt')
     os.system(f'touch ~/.cache/namaz/{x.strftime("%Y-%m-%d")}.txt')
+    print("ol")
     vakit()
     bildirim()
 
